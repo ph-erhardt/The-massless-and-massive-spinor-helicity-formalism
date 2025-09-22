e549e4be9eb63cc922d61ecfcf10b5ff3e4188a4
ph-erhardt <perhardt70@gmail.com>
2025-09-22 14:21:43 +0200

    Add files via upload

---
+++ spinor_helicity.py
@@ -0,0 +1,162 @@
+"""
+Spinor-helicity utilities for symbolic calculations in SymPy.
+
+This module implements:
+    - Pauli four-vectors and epsilon tensors for SL(2,C) and SU(2).
+    - Conversion from spherical to Cartesian coordinates.
+    - Bi-spinor equivalent of four-momenta.
+    - Construction of helicity- and spin-spinors.
+    - Spinor and tensor products.
+
+Conventions:
+    - Metric signature: (+, −, −, −).
+    - Four-vectors are given as [E, P, θ, φ] in spherical coordinates
+      unless otherwise specified.
+    - Helicity basis for spin projections
+    - HelicitySpinors(p) returns [⟨p|, |p⟩, [p|, |p]]
+    - SpinSpinors(p) returns the analogous set of spin-spinors with upper SU(2) indices.
+    
+Note:
+    - Both functions, HelicitySpinors and SpinSpinors, return ALL helicity- or spin-spinors.
+      Additionally the spinor product functions are defined such that they pick out the 
+      correct helicity- or spin-spinors when given two HelicitySpinors or SpinSpinors objects
+      So one doesn't have to define indiviual objects for each helicity- or spin-spinor which 
+      is way more convenient.
+"""
+
+from sympy import Matrix, I, eye, sin, cos, exp, sqrt
+from sympy.physics.matrices import msigma
+from sympy import tensor
+
+# Pauli 4-vectors
+sig = [eye(2), -msigma(1), -msigma(2), -msigma(3)]
+sig_bar = [eye(2), msigma(1), msigma(2), msigma(3)]
+
+# Antisymmetric 2x2 matrix with upper SL(2,C)/ little-group indices, eps^{ab}
+ueps = Matrix([
+    [0, 1],
+    [-1, 0]
+])
+
+# Antisymmetric 2x2 matrix with lower SL(2, C)/ little-group indices, eps_{ab}
+leps = Matrix([
+    [0, -1],
+    [1, 0]
+])
+
+# Useful functions
+def CartesianCoords(x):
+    """
+    Takes a python list of sympy.Symbol objects corresponding to a 4-vector in spherical coordinates [E, P, θ, φ].
+    Returns a sympy.Matrix object corresponding to the 4-vector in cartisian coordinates.
+    """
+    En, P, theta, phi = x[0], x[1], x[2], x[3]
+    return Matrix([En, P*sin(theta)*cos(phi), P*sin(theta)*sin(phi), P*cos(theta)])
+
+def MSI(x):
+    """
+    Minkowski Space Isomorphism R^4 -> C^{2x2}. 
+    
+    Takes a python list corresponding to 4-vector in cartesian coordinates.
+    Returns sympy.Matrix corresponding to the 4-vector's bi-spinor.
+    """
+    p0, p1, p2, p3 = x[0], x[1], x[2], x[3]
+    return Matrix([
+        [p0 - p3, -p1 + I*p2],
+        [-p1 - I*p2, p0 + p3]
+    ])
+
+# Definition of helicity-spinors and products
+def HelicitySpinors(x):
+    """
+    Takes a python list of sympy.Symbol objects corresponding to a 4-vector in spherical coords [E, P, θ, φ].
+    Returns a python list of sympy.Matrix objects corresponding to the 4-vector's helicity-spinors [abra, aket, sbra, sket]
+    """
+    En, P, theta, phi = x[0], x[1], x[2], x[3]
+
+    abra = sqrt(2*En) * Matrix([cos(theta/2), sin(theta/2)*exp(-I*phi)])
+    aket = sqrt(2*En) * Matrix([-sin(theta/2)*exp(-I*phi), cos(theta/2)])
+    sbra = sqrt(2*En) * Matrix([-sin(theta/2)*exp(+I*phi), cos(theta/2)])
+    sket = sqrt(2*En) * Matrix([cos(theta/2), sin(theta/2)*exp(+I*phi)])
+
+    return [abra, aket, sbra, sket]
+
+def HelicityAngleProd(A,B):
+    """
+    Computes the angle helicity-spinor product ⟨AB⟩.
+    
+    Takes two HelicitySpinors objects.
+    Returns a sympy.Matrix object corresponding to the angle helicity-spinor product of the angle helicity-spinors contained in A and B.
+    """
+    return A[0].T * B[1]
+
+def HelicitySquareProd(A,B):
+    """
+    Computes the square helicity-spinor product [AB].
+    
+    Takes two HelicitySpinors objects.
+    Returns a sympy.Matrix object corresponding to the square helicity-spinor product of the square helicity-spinors contained in A and B.
+    """
+    return A[2].T * B[3]
+
+# Definition of spin-spinors and products
+def SpinSpinors(x):
+    """
+    Takes a python list of sympy.Symbol objects corresponding to a 4-vector in spherical coords [E, P, θ, φ].
+    Returns python list of sympy.Matrix objects corresponding to the 4-vector's spin-spinors [abra, aket, sbra, sket] with *upper* SU(2) indices.
+    """
+    En, P, theta, phi = x[0], x[1], x[2], x[3]
+
+    abra = Matrix([
+        [sqrt(En - P)*exp(I*phi)*sin(theta/2), sqrt(En + P)*cos(theta/2)], 
+        [-sqrt(En - P)*cos(theta/2), sqrt(En + P)*exp(-I*phi)*sin(theta/2)]
+    ])
+    aket = Matrix([
+        [sqrt(En - P)*cos(theta/2), -sqrt(En + P)*exp(-I*phi)*sin(theta/2)], 
+        [sqrt(En - P)*exp(I*phi)*sin(theta/2), sqrt(En + P)*cos(theta/2)]
+    ])
+    sbra = Matrix([
+        [-sqrt(En + P)*exp(I*phi)*sin(theta/2), -sqrt(En - P)*cos(theta/2)], 
+        [sqrt(En + P)*cos(theta/2), -sqrt(En - P)*exp(-I*phi)*sin(theta/2)]
+    ])
+    sket = Matrix([
+        [sqrt(En + P)*cos(theta/2), -sqrt(En - P)*exp(-I*phi)*sin(theta/2)], 
+        [sqrt(En + P)*exp(I*phi)*sin(theta/2), sqrt(En - P)*cos(theta/2)]
+    ])
+
+    return [abra, aket, sbra, sket]
+
+def SpinAngleProd(A, B):
+    """
+    Computes the angle spin-spinor product ⟨AB⟩^{IJ}.
+    
+    Takes two SpinSpinors objects.
+    Returns a sympy.Matrix object corresponding to the angle spin-spinor product of the angle spin-spinors contained in A and B.
+    """
+    return A[0].T * B[1]
+
+def SpinSquareProd(A, B):
+    """
+    Computes the square spin-spinor product [AB]^{IJ}.
+    
+    Takes two SpinSpinor objects.
+    Returns a sympy.Matrix object corresponding to the square spin-spinor product of the square spin-spinors contained in A and B.
+    """
+    return A[2].T * B[3]
+
+def TensorProd(A, B):
+    """
+    Takes two sympy.Matrix objects.
+    Returns a sympy.tensor.MutableDenseNDimArray object corresponding to the tensor product of A and B
+    """
+    m, n = A.shape
+    p, q = B.shape
+    result = tensor.array.MutableDenseNDimArray.zeros(m, n, p, q)
+
+    for i in range(m):
+        for j in range(n):
+            for k in range(p):
+                for l in range(q):
+                    result[i, j, k, l] = A[i, j] * B[k, l]
+
+    return result
